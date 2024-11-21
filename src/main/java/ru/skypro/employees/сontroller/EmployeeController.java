b01fb4d24a098f97f3daf1f6975306e04e37e946
Astrasmr <135332237+Astrasmr@users.noreply.github.com>
2024-11-21 19:37:56 +0400

    Done (#1)

---
+++ src/main/java/ru/skypro/employees/сontroller/EmployeeController.java
@@ -0,0 +1,37 @@
+package ru.skypro.employees.сontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.skypro.employees.model.Employee;
+import ru.skypro.employees.service.EmployeeService;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService service) {
+        this.employeeService = service;
+    }
+
+    @GetMapping("/add")
+    public Employee addWorker(@RequestParam ("firstName") String firstName,
+                              @RequestParam ("lastName") String lastName) {
+        return employeeService.addWorker(firstName, lastName);
+    }
+
+    @GetMapping("/remove")
+    public Employee deleteWorker(@RequestParam ("firstName") String firstName,
+                                 @RequestParam ("lastName") String lastName) {
+        return employeeService.deleteWorker(firstName, lastName);
+    }
+
+    @GetMapping("/find")
+    public Employee findWorker(@RequestParam ("firstName") String firstName,
+                               @RequestParam ("lastName") String lastName) {
+        return employeeService.findWorker(firstName, lastName);
+    }
+
+}
