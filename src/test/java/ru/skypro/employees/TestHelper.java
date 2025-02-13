b29aefffb32a28f32d257a8d8e51ef18d5de87ec
katabondarenko <katushasam@rambler.ru>
2025-02-13 20:46:52 +0400

    Done

--- src/test/java/ru/skypro/employees/TestHelper.java
+++ src/test/java/ru/skypro/employees/TestHelper.java
@@ -3,11 +3,17 @@ package ru.skypro.employees;
 import com.github.javafaker.Faker;
 import ru.skypro.employees.model.Employee;
 
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
 import static org.apache.commons.lang3.RandomUtils.nextInt;
 
 public class TestHelper {
-    private static final Faker faker = new Faker();
     public static Employee getEmployee () {
-        return new Employee (faker.funnyName().name(),faker.artist().name(), nextInt(), nextInt());
+        return getEmployee(nextInt(0,1000000), nextInt(1,100));
+    }
+    public static Employee getEmployee (int salary, int department) {
+        return new Employee(randomAlphanumeric(12),
+                randomAlphanumeric(12),
+                salary,
+                department);
     }
 }
